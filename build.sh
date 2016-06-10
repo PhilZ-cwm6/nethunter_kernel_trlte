35b8d548408ca77605363cfa6fcd3d617dbebb2f
James Christopher Adduono <jc@adduono.com>
2016-06-10 19:52:30 -0400

    Build scripts

---
+++ build.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Stock kernel for Samsung Galaxy Note 4 (Qualcomm) build script by jcadduono
+
+################### BEFORE STARTING ################
+#
+# download a working toolchain and extract it somewhere and configure this
+# file to point to the toolchain's root directory.
+#
+# once you've set up the config section how you like it, you can simply run
+# DEVICE=[DEVICE] ./build.sh [VARIANT]
+#
+###################### CONFIG ######################
+
+# root directory of NetHunter trlte git repo (default is this script's location)
+RDIR=$(pwd)
+
+[ "$VER" ] ||
+# version number
+VER=$(cat "$RDIR/VERSION")
+
+# directory containing cross-compile arm toolchain
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf
+
+# amount of cpu threads to use in kernel make process
+THREADS=5
+
+############## SCARY NO-TOUCHY STUFF ###############
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
+
+[ "$DEVICE" ] || DEVICE=trlte
+[ "$TARGET" ] || TARGET=stock
+[ "$1" ] && VARIANT=$1
+[ "$VARIANT" ] || VARIANT=eur
+
+DEFCONFIG=${TARGET}_defconfig
+DEVICE_DEFCONFIG=device_${DEVICE}
+VARIANT_DEFCONFIG=variant_${DEVICE}_${VARIANT}
+
+ABORT()
+{
+	echo "Error: $*"
+	exit 1
+}
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] ||
+abort "Config $DEFCONFIG not found in $ARCH configs!"
+
+[ -f "$RDIR/arch/$ARCH/configs/$DEVICE_DEFCONFIG" ] ||
+abort "Device $DEVICE not found in $ARCH configs!"
+
+[ -f "$RDIR/arch/$ARCH/configs/$VARIANT_DEFCONFIG" ] ||
+abort "Device variant/carrier $VARIANT not found in $ARCH configs!"
+
+export LOCALVERSION=$TARGET-$DEVICE-$VARIANT-$VER
+KDIR=$RDIR/build/arch/$ARCH/boot
+
+CLEAN_BUILD()
+{
+	echo "Cleaning build..."
+	cd "$RDIR"
+	rm -rf build
+}
+
+SETUP_BUILD()
+{
+	echo "Creating kernel config for $LOCALVERSION..."
+	cd "$RDIR"
+	mkdir -p build
+	make -C "$RDIR" O=build "$DEFCONFIG" \
+		DEVICE_DEFCONFIG="$DEVICE_DEFCONFIG" \
+		VARIANT_DEFCONFIG="$VARIANT_DEFCONFIG" \
+		|| ABORT "Failed to set up build"
+}
+
+BUILD_KERNEL()
+{
+	echo "Starting build for $LOCALVERSION..."
+	while ! make -C "$RDIR" O=build -j"$THREADS"; do
+		read -p "Build failed. Retry? " do_retry
+		case $do_retry in
+			Y|y) continue ;;
+			*) return 1 ;;
+		esac
+	done
+}
+
+BUILD_DTB()
+{
+	echo "Generating dtb.img..."
+	"$RDIR/scripts/dtbTool/dtbTool" -o "$KDIR/dtb.img" "$KDIR/" -s 2048 || ABORT "Failed to generate dtb.img!"
+}
+
+CLEAN_BUILD && SETUP_BUILD && BUILD_KERNEL && BUILD_DTB && echo "Finished building $LOCALVERSION!"
