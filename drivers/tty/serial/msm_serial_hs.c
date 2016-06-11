187ec761f26214af2065576043e6fbf52a907693
James Christopher Adduono <jc@adduono.com>
2016-06-10 20:55:32 -0400

    Compiler warning fixes

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -75,9 +75,7 @@ static void *ipc_msm_hs_log_ctxt;
 #define IPC_MSM_HS_LOG_PAGES 30
 
 
-#define _DW_ENABLED
-
-#ifdef _DW_ENABLED
+#ifdef CONFIG_DUALWAVE
 #define MAX_DUALWAVE_MESSAGE_SIZE 128
 
 #include <linux/syscalls.h>
@@ -1624,7 +1622,7 @@ static void msm_serial_hs_rx_tlet(unsigned long tlet_ptr)
 			msm_uport->rx.buffer_pending |= CHARS_NORMAL |
 				retval << 5 | (rx_count - retval) << 16;
 		}
-	#ifdef _DW_ENABLED
+	#ifdef CONFIG_DUALWAVE
 		else
 		{
 			if (checkDualWaveStatus() != DUALWAVE_INACTIVE) {
--- drivers/tty/serial/msm_serial_hs_lentis.c
+++ drivers/tty/serial/msm_serial_hs_lentis.c
@@ -74,9 +74,7 @@ static void *ipc_msm_hs_log_ctxt;
 #define IPC_MSM_HS_LOG_PAGES 30
 #define UART_DMA_DESC_NR 8
 
-#define _DW_ENABLED
-
-#ifdef _DW_ENABLED
+#ifdef CONFIG_DUALWAVE
 #define MAX_DUALWAVE_MESSAGE_SIZE 128
 
 #include <linux/syscalls.h>
@@ -1795,13 +1793,9 @@ static void msm_serial_hs_rx_tlet(unsigned long tlet_ptr)
 					CHARS_NORMAL | retval << 5 |
 					(rx_count - retval) << 16;
 				}
-#ifdef _DW_ENABLED
-                else
-                {
-                    if (checkDualWaveStatus() != DUALWAVE_INACTIVE) {
-                        UpdateTime(msm_uport->rx.buffer, rx_count);
-                    }
-                }
+#ifdef CONFIG_DUALWAVE
+				else if (checkDualWaveStatus() != DUALWAVE_INACTIVE)
+					UpdateTime(msm_uport->rx.buffer, rx_count);
 #endif
 			} else
 				MSM_HS_ERR("Error in inx %d",
